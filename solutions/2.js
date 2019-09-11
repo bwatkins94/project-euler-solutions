c92e3f380d19e632def1b10fd09cb118d21addce
Eagle0ne <brucewatkins95@gmail.com>
2019-09-11 01:00:37 -0400

    Solution for Challenge Two

---
+++ solutions/2.js
@@ -0,0 +1,19 @@
+"use strict"
+
+  var previous = 0;
+  var current = 1;
+  var sum = 0;
+  var next;
+
+  for(var i = 1; i < 100; i++) {
+    next = current + previous;
+    previous = current;
+    current = next;
+    if(current % 2 === 0 && current < 4000000) {
+        sum += current;
+      console.log(sum);
+    }
+  }
+
+
+ module.exports = sum;
